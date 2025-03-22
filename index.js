bb0884686a52a4db4dd766b8e1e557183c0de386
Thalison Rabelo <rabelo.thalison@gmail.com>
2025-03-22 14:38:49 -0300

    Atualizadas as msg do sistema

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ import { turboLogger } from "./turbologger.js";
 console.log("usando o gitHub");
 
 turboLogger("Aprendendo fluxo do git");
+turboLogger("Criando uma nova msg sem mexer na versão principal", "darkgreen");
